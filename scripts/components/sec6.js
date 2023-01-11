e2113a7b9475b7046a48e7b0319345f2f180c61f
Eslam Gamal <geslam587@gmail.com>
2023-01-11 20:49:00 +0200

    finishing section 5 and  section 6

--- scripts/components/sec6.js
+++ scripts/components/sec6.js
@@ -3,7 +3,29 @@ import comket from '../../comket/html.js'
 
 export default function sec6() {
     let parent = comket.div({
-        class:"section6"
+        class:"section6",
+        children:[
+            comket.div({
+                class:"content",
+                children:[
+                    comket.div({ class:"head",
+                    children:[
+                        comket.img({src:"../../media/circle.svg"}),
+                        comket.p({class:"title", text:"Ed-Circle."})
+                    ]
+                }),
+                comket.p({class:"courses",text:"Courses was fantastic! It is Master platform for those looking to start a new career, or need a refresher."}),
+                comket.div({
+                    class:"jacop",
+                    children:[
+                        comket.img({src:"../../media/jacop.svg"}),
+                        comket.p({ class:"name", text:"Jacob Jones"}),
+                        comket.p({ class:"discription", text:"Student, National University"}),
+                    ]
+                })
+                ]
+            })
+        ]
 
     })
     // build your component in this function
